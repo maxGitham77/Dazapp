d5e7e22433e977f765ec85b266a8a156de3aeae0
Maxwell IG <m.imasuen-goertzen@perfect-media-solutions.de>
2024-01-21 08:08:35 +0100

    Handled uncaughtException, unhandledRejection errors

--- src/index.js
+++ src/index.js
@@ -4,6 +4,34 @@ import logger from "./config/logger.config.js";
 // env variables
 const PORT = process.env.PORT || 8009;
 
-app.listen(PORT, () => {
+let server;
+
+server = app.listen(PORT, () => {
 	logger.info(`Server is listening at ${PORT}`);
+	//throw new Error("error in server");
+	console.log("process id", process.pid);
+});
+
+// handle server errors
+const exitHandler = () => {
+	if (server) {
+		logger.info("Server closed.");
+		process.exit(1);
+	}
+};
+
+const unexpectedErrorHandler = (error) => {
+	logger.error(error);
+	exitHandler();
+};
+
+process.on("uncaughtException", unexpectedErrorHandler);
+process.on("unhandledrejection", unexpectedErrorHandler);
+
+// SIGTERM
+process.on("SIGTERM", () => {
+	if (server) {
+		logger.info("Server closed.");
+		process.exit(1);
+	}
 });
